4f7ff018f9269b03c5158736c124062b92e86c7d
Eliza <ekoknapp@gmail.com>
2021-10-30 15:09:55 -0400

    basic main page

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,18 @@
-#Team fourCoffeePeanuts: Ryan Wang (PM), Eliza Knapp, Yaying Liang Li, Jesse Xie
-#SoftDev
-#P00 -- Move Slowly and Fix Things
+# Team fourCoffeePeanuts: Ryan Wang (PM), Eliza Knapp, Yaying Liang Li, Jesse Xie
+# SoftDev
+# P00 -- Move Slowly and Fix Things
 
-from flask import Flask, render_template #Q0: What happens if you remove render_template from this line?
-app = Flask(__name__)
+# setup 
+from flask import Flask, render_template, request, redirect, session # flask imports
+from flask_session import Session # creates a session instance
+app = Flask(__name__) #creates flask object
+
+app.secret_key = "random" # TODO: perhaps change to urandom(32)
 
 @app.route("/")
-def hello_world():
-    return "No hablo queso!"
+def main_page():
+    return render_template("mainPage.html")
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
