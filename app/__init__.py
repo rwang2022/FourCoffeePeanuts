c389a891be94638023afd73a9bc66c229573a164
Eliza <ekoknapp@gmail.com>
2021-10-30 15:34:37 -0400

    pseudocode for the submit functions

--- app/__init__.py
+++ app/__init__.py
@@ -20,10 +20,34 @@ def main_page():
 def create_account():
     return render_template("login_create.html", create=True)
 
+# handles submitting of create account
+@app.route("/submit_create_account")
+def submit_create_account():
+    # try 
+    if request.method == "POST":
+        # check if username is in the database
+            # if it is, return this username has been taken error
+            # if it is not, check if the passwords match
+                # if they do, add the entry to the database
+                # if they do not, return passwords do not match error
+    # overall catch here to make sure site never breaks
+
 # login site
 @app.route("/login")
 def login():
     return render_template("login_create.html", create=False)
+
+# handles submitting of login
+@app.route("/submit_login")
+def submit_login():
+    # try
+    if request.method == "POST":
+        # check if username is in database
+            # if it is, check password
+                # if everything works, log the user in successfully
+                # if the password is wrong, return that error
+            # if the username is not, return username wrong error
+    # overall catch for working site
 
 if __name__ == "__main__":
     app.debug = True
