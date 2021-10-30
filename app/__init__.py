7caceb99dcf53a2cec627ce16ae8b2bd68d4a1c1
Eliza <ekoknapp@gmail.com>
2021-10-30 15:27:47 -0400

    login and create pages

--- app/__init__.py
+++ app/__init__.py
@@ -7,11 +7,23 @@ from flask import Flask, render_template, request, redirect, session # flask imp
 from flask_session import Session # creates a session instance
 app = Flask(__name__) #creates flask object
 
+# TODO: configure session stuff
 app.secret_key = "random" # TODO: perhaps change to urandom(32)
 
+# renders the main page
 @app.route("/")
 def main_page():
-    return render_template("mainPage.html")
+    return render_template("main_page.html")
+
+# create account site
+@app.route("/create_account")
+def create_account():
+    return render_template("login_create.html", create=True)
+
+# login site
+@app.route("/login")
+def login():
+    return render_template("login_create.html", create=False)
 
 if __name__ == "__main__":
     app.debug = True
