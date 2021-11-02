79515dfde047436a47a8f8266f6f10c7cfdd5831
Eliza <ekoknapp@gmail.com>
2021-11-02 11:14:29 -0400

    added try catch to make sure no errors

--- app/__init__.py
+++ app/__init__.py
@@ -38,41 +38,70 @@ def create_account():
 # handles submitting of create account
 @app.route("/submit_create_account", methods=['GET', 'POST'])
 def submit_create_account():
-    # try
-    if request.method == "POST":
-        username = request.form.get("username")
-        password = request.form.get("password")
-        same_password = request.form.get("same_password")
-        #get data from form
-        c.execute("SELECT * FROM users")
-        usersTable = c.fetchall()
-        #fetch user table data from db file
-        if username == '':
-            return render_template("login_create.html", create=True, error="Your username cannot be blank")
-        userTaken = False
-        for user in usersTable: # check if username is in the database
-            if user[0] == username:
-                userTaken = True
-        if userTaken:
-            return render_template("login_create.html", create=True, error="That username has already been taken")
-            # if it is, return this username has been taken error
-        elif password != same_password: # if it is not, check if the passwords match
-            return render_template("login_create.html", create=True, error="The passwords do not match")
-            # if they do not, return passwords do not match error
-        elif password == '':
-            return render_template("login_create.html", create=True, error="Your password cannot be blank")
-        else:
-            info = [username,password,""]
-            addAccount = f"INSERT INTO users VALUES(?,?,?)" # if they do, add the entry to the database
-            c.execute(addAccount,info) #add user data to table
-            db.commit() #save changes
-            return main_page()
-    # overall catch here to make sure site never breaks
+    try:
+        if request.method == "POST":
+            username = request.form.get("username")
+            password = request.form.get("password")
+            same_password = request.form.get("same_password")
+            #get data from form
+            c.execute("SELECT * FROM users")
+            usersTable = c.fetchall()
+            #fetch user table data from db file
+            if username == '':
+                return render_template("login_create.html", create=True, error="Your username cannot be blank")
+            userTaken = False
+            for user in usersTable: # check if username is in the database
+                if user[0] == username:
+                    userTaken = True
+            if userTaken:
+                return render_template("login_create.html", create=True, error="That username has already been taken")
+                # if it is, return this username has been taken error
+            elif password != same_password: # if it is not, check if the passwords match
+                return render_template("login_create.html", create=True, error="The passwords do not match")
+                # if they do not, return passwords do not match error
+            elif password == '':
+                return render_template("login_create.html", create=True, error="Your password cannot be blank")
+            else:
+                info = [username,password,""]
+                addAccount = f"INSERT INTO users VALUES(?,?,?)" # if they do, add the entry to the database
+                c.execute(addAccount,info) #add user data to table
+                db.commit() #save changes
+                return dashboard()
+    except:
+        return render_template("login_create.html", create=True, error="Error!") # overall error catch
 
 # login page
 @app.route("/login")
 def login():
     return render_template("login_create.html", create=False)
+
+# handles submitting of login
+@app.route("/submit_login", methods=['GET', 'POST'])
+def submit_login():
+    try:
+        if request.method == "POST":
+            user = request.form.get("username") #get data from form
+            passwd = request.form.get("password")
+
+            c.execute("SELECT * FROM users")
+            usersTable = c.fetchall() #fetch user table data from db file;
+            #usersTable is a list of tuples: [(user,pass,stories), (user,pass,stories)]
+
+            for value in usersTable:
+                if value[0] == user: #check if user is in the users database
+                    if value[1] == passwd: #if user is, check if password is correct
+                        global logged_in_user
+                        logged_in_user = user
+                        session[user] = passwd
+                        return dashboard() #if everything works, log the user in successfully
+                    else: #user exists, but password is wrong
+                        return render_template("login_create.html", create=False, error="The password is incorrect") #call error fxn; indicate passwd is incorrect
+            return render_template("login_create.html", create=False, error="That user does not exist") #call error fxn; indicate username is incorrect
+            #only return this after checking all the usernames & confirming it doesn't exist
+    except:
+        return render_template("login_create.html", create=True, error="Error!")
+        #overall catch for working site
+        #c.execute("SELECT password from users WHERE username=user")
 
 #logout page
 @app.route("/logout", methods=['GET', 'POST'])
@@ -82,42 +111,6 @@ def logout():
         session.pop(logged_in_user)
         logged_in_user = ""
     return render_template('main_page.html')
-
-# handles submitting of login
-@app.route("/submit_login", methods=['GET', 'POST'])
-def submit_login():
-    # try
-    if request.method == "POST":
-        user = request.form.get("username") #get data from form
-        passwd = request.form.get("password")
-
-        c.execute("SELECT * FROM users")
-        usersTable = c.fetchall() #fetch user table data from db file;
-        #usersTable is a list of tuples: [(user,pass,stories), (user,pass,stories)]
-
-        for value in usersTable:
-            if value[0] == user: #check if user is in the users database
-                if value[1] == passwd: #if user is, check if password is correct
-                    global logged_in_user
-                    logged_in_user = user
-                    session[user] = passwd
-                    return dashboard() #if everything works, log the user in successfully
-                else: #user exists, but password is wrong
-                    return render_template("login_create.html", create=False, error="The password is incorrect") #call error fxn; indicate passwd is incorrect
-        return render_template("login_create.html", create=False, error="That user does not exist") #call error fxn; indicate username is incorrect
-        #only return this after checking all the usernames & confirming it doesn't exist
-        #overall catch for working site
-
-        #c.execute("SELECT password from users WHERE username=user")
-
-    '''
-        # check if username is in database
-            # if it is, check password
-                # if everything works, log the user in successfully
-                # if the password is wrong, return that error
-            # if the username is not, return username wrong error
-    # overall catch for working site
-    '''
 
 @app.route("/dashboard")
 def dashboard():
